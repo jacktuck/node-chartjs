960b32bf58a4041116d1275a4076cf34ae43a27a
jt <jack.tuck@crugo.com>
2018-01-31 20:00:06 +0000

    test

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var Canvas = require('canvas-prebuilt')
 var pify = require('pify')
 var config = require('./config')
 var opn = require('opn')
-
+const chartjs = require('chart.js')
 var env = pify(jsdom.env)
 
 ;(async function (exports) {
-  var chart = 'https://cdn.jsdelivr.net/npm/chart.js@2.4/dist/Chart.min.js'
+  // var chart = 'https://cdn.jsdelivr.net/npm/chart.js@2.4/dist/Chart.min.js'
 
   var html = `<html>
     <body>
@@ -20,7 +20,7 @@ var env = pify(jsdom.env)
   </html>`
 
   try {
-    var window = await env(html, [chart], {
+    var window = await env(html, null, {
       features: {
         FetchExternalResources: ['script'],
         ProcessExternalResources: ['script'],
@@ -28,7 +28,7 @@ var env = pify(jsdom.env)
       }
     })
 
-    window.CanvasRenderingContext2D = Canvas.Context2d
+    // window.CanvasRenderingContext2D = Canvas.Context2d
 
     config.options.responsive = false
     config.animation = false
@@ -46,8 +46,33 @@ var env = pify(jsdom.env)
     // _ctx.beginPath()
     // _ctx.arc(95,50,40,0,2*Math.PI)
     // _ctx.stroke()
+    this._window = window;
+    // const canvas = require('canvas');
+    const canvasMethods = ['HTMLCanvasElement'];
 
-    new window.Chart(_ctx, config)
+    // adding window properties to global (only properties that are not already defined).
+    this._windowPropertiesToDestroy = [];
+    Object.keys(window).forEach(property => {
+        if (typeof global[property] === 'undefined') {
+            if (typeof global[property] === 'undefined') {
+                global[property] = window[property];
+                this._windowPropertiesToDestroy.push(property);
+            }
+        }
+    });
+
+    // adding all window.HTMLCanvasElement methods to global.HTMLCanvasElement
+    canvasMethods.forEach(method =>
+        global[method] = window[method]
+    );
+
+    // global.CanvasRenderingContext2D = canvas.Context2d;
+
+    global.navigator = {
+        userAgent: 'node.js'
+    };
+
+    const _chrt = new chartjs(_ctx, config)
 
     _canvas.toBlob(function (blob, err) {
       console.log('ERR', err)
